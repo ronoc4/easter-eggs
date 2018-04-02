76b59f020921e6729a74973269b123d732750b64
Conor Budge <conorbudge555@gmail.com>
2018-04-02 08:53:14 -0500

    Cleaned up unused input field, and CSS updates

--- main.js
+++ main.js
@@ -7,30 +7,20 @@ const CANVAS = document.getElementById('ee-canvas');
 const ctx = CANVAS.getContext('2d');
 const radiusX = 65;
 const radiusY = 90;
-const rotation = 180 * Math.PI/180;
+const rotation = 180 * Math.PI / 180;
 const startAngle = 0;
-const endAngle = 2* Math.PI;
+const endAngle = 2 * Math.PI;
 let cx, cy;
-let userInput = "";
-let totalEggs = getUserInput();
-
 let userClick = document.getElementById("add-eggs-btn");
 
 window.onload = function () {
     userClick.onclick = function () {
-        console.log(totalEggs);
         draw();
     };
 };
 
-function getUserInput() {
-    userInput = document.getElementById("txtNumEggs").value;
-    return 12;
-}
-
-//Create the egg shape
 function drawEgg() {
-    for(let i = 0; i < totalEggs; i++) {
+    for (let i = 0; i < 10; i++) {
         ctx.strokeStyle = randomColor();
         ctx.fillStyle = randomColor();
         cx = Math.random() * 500;
@@ -46,15 +36,13 @@ function drawEgg() {
     }
 }
 
-//Draw the eggs on the canvas
 function draw() {
-    ctx.clearRect(0,0, CANVAS.width, CANVAS.height);
+    ctx.clearRect(0, 0, CANVAS.width, CANVAS.height);
     drawEgg();
 }
 
-//Change to random colors
 function randomColor() {
     let o = Math.round, r = Math.random, s = 255;
-    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+    return 'rgba(' + o(r() * s) + ',' + o(r() * s) + ',' + o(r() * s) + ',' + r().toFixed(1) + ')';
 }
 
